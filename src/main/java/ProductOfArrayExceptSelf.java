84c0712d411cb29587e26522fc13b6c5c29a216d
danil <ivanchikdanil@gmail.com>
2023-08-24 15:47:39 +0300

    In this commit, we have successfully solved the 'Product of Array Except Self' problem. Our solution boasts impressive results, with a runtime of just 1ms, outperforming 100.00% of users using Java. Additionally, our memory usage is optimized, ranking higher than 55.93% of Java users + Tests

--- src/main/java/ProductOfArrayExceptSelf.java
+++ src/main/java/ProductOfArrayExceptSelf.java
@@ -1,6 +1,33 @@
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelf {
 
+    //Runtime: 1ms
+    //Beats 100.00%of users with Java
+
+    //Memory: 52.83MB
+    //Beats 55.93%of users with Java
+
     public int[] productExceptSelf(int[] nums) {
-        return null;
+
+        if (nums.length ==1){
+            return nums;
+        }
+
+        int n = nums.length;
+        int mult = 1;
+        int[] ans = new int[n];
+        for(int i = 0; i<n; i++){
+            ans[i] = mult;
+            mult*=nums[i];
+        }
+
+        mult = 1;
+        for(int j = n-1; j>=0; j--){
+            ans[j]*=mult;
+            mult*=nums[j];
+        }
+        return ans;
+
     }
 }
--- src/test/java/ProductOfArrayExceptSelfTest.java
+++ src/test/java/ProductOfArrayExceptSelfTest.java
@@ -1,5 +1,38 @@
 import junit.framework.TestCase;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ProductOfArrayExceptSelfTest {
+
+    @Test
+    public void ProductOfArrayExceptSelfStandartTest(){
+        ProductOfArrayExceptSelf product = new ProductOfArrayExceptSelf();
+        assertArrayEquals(new int[] {24,12,8,6},product.productExceptSelf(new int[]{1,2,3,4}));
+    }
+
+    @Test
+    public void ProductOfArrayExceptSelfBigLengthTest(){
+        ProductOfArrayExceptSelf product = new ProductOfArrayExceptSelf();
+        int[] intArr = new int[100000];
+        for (int i = 0; i < intArr.length; i++){
+            intArr[i] = 1;
+        }
+        assertArrayEquals(intArr,product.productExceptSelf(intArr));
+    }
+
+    @Test
+    public void ProductOfArrayExceptSelfEmptyTest(){
+        ProductOfArrayExceptSelf product = new ProductOfArrayExceptSelf();
+        assertArrayEquals(new int[0],product.productExceptSelf(new int[0]));
+    }
+
+    @Test
+    public void ProductOfArrayExceptSelfOneLengthTest(){
+        ProductOfArrayExceptSelf product = new ProductOfArrayExceptSelf();
+        assertArrayEquals(new int[1],product.productExceptSelf(new int[1]));
+    }
+
+
 
 }
